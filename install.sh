8a78a1942724b6d308bdd9ace19e6be83b9c26e2
01-Teste <59323037+01-Teste@users.noreply.github.com>
2019-12-29 06:26:01 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,7 +10,8 @@ if [[ ! -d '/etc/rftools' ]]; then mkdir /etc/rftools; fi
 if [[ ! -f '/usr/local/bin/rftools' ]]; then echo "Configurar o PATH do programa"; fi
 
 # Configurando os arquivos para o funcionamento do sistema
-cp -r /home/rafael/bin/PROGRAMAS/RFTOOLS/{LIB,VARS,README,rftools,install.sh,uninstall.sh} /etc/rftools
+cp -r ./{LIB,VARS,README,install.sh,uninstall.sh} /etc/rftools
+cṕ -r ./rtools /usr/local/bin/ ; chmod +x /usr/local/bin/rftools
 
 # Erros
 if [[ ! -d '/etc/rftools/LIB' ]]; then echo "AS BIBLIOTECAS NÂO FORAM INSTALADAS"; fi
